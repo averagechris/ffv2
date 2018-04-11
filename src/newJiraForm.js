28b85164b3415d431a5103ed975619c2e4b8980b
mistahchris <mistahcummings@gmail.com>
2018-04-10 23:18:00 -0500

    add ReferenceItem hoc rendering to JiraForm

--- src/newJiraForm.js
+++ src/newJiraForm.js
@@ -1,50 +1,103 @@
 import React, { Component } from "react";
-import { Button, Form, FormInput } from "basic-react-component-library";
+import {
+  Button,
+  CloseIcon,
+  Form,
+  FormInput,
+  TagItem
+} from "basic-react-component-library";
+
+const ReferenceItem = ({ value, onClick, toRemove, ...props }) => (
+  <TagItem
+    additionalContainerClasses={["ma1", "blue"]}
+    additionalValueClasses={["dib underline-hover"]}
+    name={value}
+    value={value}
+    onClick={onClick}
+    {...props}
+  >
+    <CloseIcon
+      additionalContainerClasses={["dim"]}
+      color="gray"
+      size="small"
+      onClick={toRemove}
+    />
+  </TagItem>
+);
 
 class JiraForm extends Component {
   constructor() {
     super();
     this.state = {
       references: {
-        emails: [],
-        eventIds: [],
-        orderIds: []
+        emails: new Set(),
+        eventIds: new Set(),
+        orderIds: new Set()
       }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.setReferences = this.setReferences.bind(this);
+    this.addReferenceItems = this.addReferenceItems.bind(this);
+    this.removeReferenceItem = this.removeReferenceItem.bind(this);
     this.transformationsOnChange = this.transformationsOnChange.bind(this);
   }
-
-  handleSubmit(formData) {
-    console.log(formData);
+  _formatReferencesToMarkupString({ emails, eventIds, orderIds }) {
+    let adminUrl = "https://admin.eventbrite.com/?q=";
+    let emailStr = [...emails].reduce(
+      (t, email) =>
+        (t += `[${email}|${adminUrl}${encodeURIComponent(email)}]\n`),
+      ""
+    );
+    let eventIdStr = [...eventIds].reduce(
+      (t, eid) => (t += `eid: [${eid}|${adminUrl}${eid}]\n`),
+      ""
+    );
+    let orderIdStr = [...orderIds].reduce(
+      (t, oid) => (t += `oid: [${oid}|${adminUrl}${oid}]\n`),
+      ""
+    );
+    // TODO: orderId admin url is _NOT_ the same
+    return emailStr + eventIdStr + orderIdStr;
   }
-
-  setReferences(value) {
+  handleSubmit(formData) {
+    let references = this._formatReferencesToMarkupString(
+      this.state.references
+    );
+    console.log(references);
+    let data = { ...formData, References: references };
+    console.log(data);
+  }
+  addReferenceItems(value) {
     let newEmails =
       value.match(/\b[a-z.+-_]+@[a-z.+-_]+(?:\.\w+){1,3}\b/g) || [];
     let newEventIds = value.match(/\b\d{11,12}\b/g) || [];
     let newOrderIds = value.match(/\b\d{8,10}\b/g) || [];
     this.setState(s => {
-      let emails = new Set([...s.references.emails, ...newEmails]);
-      let eventIds = new Set([...s.references.eventIds, ...newEventIds]);
-      let orderIds = new Set([...s.references.orderIds, ...newOrderIds]);
       return {
         ...s,
         references: {
-          emails: [...emails],
-          eventIds: [...eventIds],
-          orderIds: [...orderIds]
+          emails: new Set([...s.references.emails, ...newEmails]),
+          eventIds: new Set([...s.references.eventIds, ...newEventIds]),
+          orderIds: new Set([...s.references.orderIds, ...newOrderIds])
         }
       };
     });
   }
+  removeReferenceItem(type, value) {
+    this.setState(s => {
+      let newReferences = new Set([...s.references[type]]);
+      newReferences.delete(value);
 
+      return {
+        ...s,
+        references: { ...s.references, [type]: newReferences }
+      };
+    });
+  }
   transformationsOnChange() {
     let transformationFunctions = [
       ({ fieldName, value }) => {
         if (fieldName === "References") {
-          this.setReferences(value);
+          this.addReferenceItems(value);
           return "";
         }
       }
@@ -52,6 +105,7 @@ class JiraForm extends Component {
     return transformationFunctions;
   }
   render() {
+    let { references } = this.state;
     return (
       <Form
         onSubmit={this.handleSubmit}
@@ -65,7 +119,29 @@ class JiraForm extends Component {
           name="References"
           placeholder="paste account emails, event or order ids"
         />
-
+        <div>
+          {[...references.emails].map((r, i) => (
+            <ReferenceItem
+              key={`reference-email-${r}-${i}`}
+              value={r}
+              toRemove={() => this.removeReferenceItem("emails", r)}
+            />
+          ))}
+          {[...references.eventIds].map((r, i) => (
+            <ReferenceItem
+              key={`reference-event-${r}-${i}`}
+              value={`oid: ${r}`}
+              toRemove={() => this.removeReferenceItem("eventIds", r)}
+            />
+          ))}
+          {[...references.orderIds].map((r, i) => (
+            <ReferenceItem
+              key={`reference-order-${r}-${i}`}
+              value={`eid: ${r}`}
+              toRemove={() => this.removeReferenceItem("orderIds", r)}
+            />
+          ))}
+        </div>
         <div className="tc">
           <Button text="SUBMIT" onClick={() => {}} />
         </div>
